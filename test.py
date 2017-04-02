5eda5ec3e762dda6a89735071cd189f2221aab14
KaspArno <kaspersk@student.matnat.uio.no>
2017-04-02 16:30:03 +0200

    brancheTest

--- test.py
+++ test.py
@@ -30,6 +30,7 @@ from test_dockwidget import TestDockWidget
 import os.path
 
 #This is a test line for Git hub, this is a great line
+#Branche test
 class Test:
     """QGIS Plugin Implementation."""
 
